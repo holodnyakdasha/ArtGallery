66dbcec031f5266d33b022f7314fe4a8c3f434d8
holodnyakdasha <holodnyakdasha@gmail.com>
2018-10-14 19:41:53 +0300

    Initial commit
    first mark

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+// $(document).ready(function(){
+//     $('.menu-toggle').on('click',function(){
+//         $('.menu-toggle').toggleClass('active');
+//     });
+// });
+
+var toggle = document.getElementById('toggle');
+
+
+toggle.addEventListener('click', function () {
+
+
+
+});
+
+
+

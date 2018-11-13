6b9a0e2db87bf79d3502e9f1152d14c86b14f34d
holodnyakdasha <holodnyakdasha@gmail.com>
2018-11-13 16:19:01 +0200

    fixed popup menu

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,8 @@ let popup_menu = $('#popup');
 let burger = $('#burger');
 
 function toggleBurger() {
-    popup_menu.hasClass('hidden') ? popup_menu.removeClass('hidden') : popup_menu.addClass('hidden')
+    popup_menu.hasClass('hidden')?popup_menu.removeClass('hidden'): popup_menu.addClass('hidden');
+
 }
 
 burger.on('click', toggleBurger);
